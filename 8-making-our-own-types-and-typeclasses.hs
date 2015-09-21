b0fdf75005d13007728711cc5275f5f9932cd15c
Innocent <grahamin@60f81db59b6c.ant.amazon.com>
2015-09-21 14:44:40 +0100

    ch 8 basics

--- 8-making-our-own-types-and-typeclasses.hs
+++ 8-making-our-own-types-and-typeclasses.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 {-
  - We are going to create some types for a deck of cards
  - The cards need to have an ordering, based on the standard ranking http://en.wikipedia.org/wiki/Standard_52-card_deck#Rank_and_color
@@ -12,28 +14,34 @@
  - Uncomment the following declarations to complete the implementation, and provide an implementation for instance Show Card
  -}
 
---data Suit = 
---data Digit = 
---data Card = 
+data Suit = Hearts | Clubs | Diamonds | Spades deriving (Eq, Show) 
+data Digit = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King deriving (Ord, Eq,Show)
+data Card = Card Suit Digit | Joker deriving (Eq, Show) 
+instance Ord Card where
+	(Card _ s1) `compare` (Card _ s2) = s1 `compare` s2
+
+-- This is wrong because I have violated the 'total order' contract of Ord
 
 -- We should be able to provide a function which returns the higher ranked card:
 betterCard :: Card -> Card -> Card
-betterCard x y = undefined
+betterCard x y = maximum [x, y]
+
+-- What I should have done is implement the partial Ord here
 
 -- Here is a new Typeclass, which represents some kind of playing hand in a game.
 -- It returns True for a "winning hand", depending on the rules for the type of class we are playing with
 class Hand a where
     play :: [a] -> Bool
 
+
 -- Implement Hand for Card, where play returns true if the list contains the Ace of Spades
 instance Hand Card where
-    play c = undefined
+    play c = (Card Spades Ace) `elem` c
 
 -- Create a new Coin type
---data Coin = 
+data Coin = Head | Tail deriving (Eq, Show)
 
 -- Implement Hand for Coin, where play returns true if there are ten heads in a row in the list
 instance Hand Coin where
-	play c =  undefined
-
--- Have a play with implementing Hand for some other types, for instance Int and Bool
+   play c = (replicate 10 Head)  `isInfixOf` c 
+   	
