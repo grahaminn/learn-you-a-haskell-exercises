0e2ad939521b82ae8ff1c230faef13e65df2dd3e
Innocent <grahamin@60f81db59b6c.ant.amazon.com>
2015-11-03 12:39:59 +0000

    progress on 9 and 11

--- 11-functors-applicative-functors-and-monoids.hs
+++ 11-functors-applicative-functors-and-monoids.hs
@@ -5,22 +5,34 @@ import Data.Monoid
 data List a = Empty | Value a (List a) deriving (Show)
 
 -- Make the list a Functor
+instance Functor List where
+	fmap f Empty = Empty
+	fmap f (Value a l) = Value (f a ) (fmap f l)
 
 -- Write a function which appends one list on to another
 combineLists:: List a -> List a -> List a
-combineLists a b = undefined
+combineLists Empty b = b
+combineLists a Empty = a
+combineLists (Value a l) b = Value a (combineLists l b) 
 
 -- Make our list a Monoid
+instance Monoid List where
+	mempty = Empty
+	mappend = combineLists
 
 -- Make our list an Applicative
+instance Applicative List where
+	pure x = Value x
+	fs <*> ls = fmap fs ls 
 
 -- Make sure that the List obeys the laws for Applicative and Monoid
 
 -- Create some lists of numbers of different lengths such as:
 twoValueList = Value 10 $ Value 20 Empty
+fourValueList = Value 4 $ Value 17 $ Value 5 $ Value 8 Empty
 
 -- Use <$> on the lists with a single-parameter function, such as:
-plusTwo = (+2)
+plusTwo = (+2) <$> fourValueList
 
 -- Use <$> and <*> on the lists with a binary function
 
--- 9-input-and-output.hs
+++ 9-input-and-output.hs
@@ -3,8 +3,17 @@
  - The program arguments are simply printed to the standard output.
  - If the first argument is -n, this argument is not printed, and no trailing newline is printed
  -}
-  
-main = undefined
+
+import Data.List
+import System.Environment
+import System.Random
+
+main :: IO ()
+main = do
+	x <- getArgs
+	if x !! 0 == "-n"
+		then do putStr (unwords(drop 1 x))
+		else do putStrLn (unwords x)	
 
 {- Write a lottery number picker
  - This function should take a StdGen instance, and produce a list of six unique numbers between 1 and 49, in numerical order
