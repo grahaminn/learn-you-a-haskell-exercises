b2bafa42e2e7ce7cf0a9451a521073a7c9478bb8
grahaminn <grahami@blinkbox.com>
2015-11-15 00:21:31 +0000

    ch 12 exercises done

--- 12-a-fistful-of-monads.hs
+++ 12-a-fistful-of-monads.hs
@@ -5,27 +5,33 @@
  -   Success, which takes that parameter and
  -   Fail String, which represents a failure, and a reason for that failure
  -}
---data Validation a 
+data Validation a = Success a | Fail String 
 
 -- Make the Validation a Monad
-
+instance Monad Validation where
+	return a = Success a
+	Success a >>= f = f a
+	Fail a >>= f = Fail a
+	fail message = Fail message
 
 {-
  - Create a function, positiveCheck, which takes a number and returns a successful Validation if it's positive, 
  - and a failed Validation with a String message if not.
  -}
---positiveCheck :: (Num a, Ord a) => a -> Validation a
---positiveCheck x = undefined
+positiveCheck :: (Num a, Ord a) => a -> Validation a
+positiveCheck x | x < 0 = fail "x is negative"
+				| x >= 0 = return x 
 
 {-
  - Create a function, evenCheck, which returns a successful Validation if it's even,
  - and a failed Validation with a string message if it's odd
  -}
---evenCheck :: (Integral a)  =>  a -> Validation a
---evenCheck x = undefined
+evenCheck :: (Integral a)  =>  a -> Validation a
+evenCheck x | mod x 2 == 0 = return x
+			| mod x 2 == 1 = fail "x is an odd number"
 
 {-
  - Write a function which uses positiveCheck and evenCheck to make sure a number is both positive and even
  -}
---positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
---positiveAndEvenCheck x = undefined
+positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
+positiveAndEvenCheck x = (evenCheck x)  >>= positiveCheck
